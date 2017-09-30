debf42a9c7df551ea8665b634d3ac039c0d2d7f1
Jared Hanson <jaredhanson@gmail.com>
2017-09-29 17:47:35 -0700

    Remove unused mocks.

--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,6 @@
 var chai = require('chai');
 var robots = require('../lib');
 
-function MockSite() {
-  this.settings = {};
-  this.pages = {};
-}
-
-MockSite.prototype.get =
-MockSite.prototype.set = function(setting, val) {
-  if (1 == arguments.length) {
-    return this.settings[setting];
-  } else {
-    this.settings[setting] = val;
-    return this;
-  }
-}
-
-MockSite.prototype.page = function(path, fn) {
-  this.pages[path] = new MockPage(path, fn);
-}
-
-function MockPage(path, fn) {
-  this.path = path;
-  this.fn = fn;
-  this.data = '';
-}
-
-MockPage.prototype.write = function(data) {
-  this.data += data;
-}
-
 
 describe('kerouac-robotstxt', function() {
   
