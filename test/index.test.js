0dcc58ca18d76de6a5aab660612d4c3d435002af
Jared Hanson <jaredhanson@gmail.com>
2022-05-21 16:25:33 -0700

    Fix tests.

--- test/index.test.js
+++ test/index.test.js
@@ -5,187 +5,77 @@ var robots = require('../lib');
 
 describe('kerouac-robotstxt', function() {
   
-  it('should export function', function() {
-    expect(robots).to.be.a('function');
-  });
+  it('should write robots.txt', function(done) {
+    chai.kerouac.use(robots())
+      .finish(function() {
+        expect(this.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\n');
+        done();
+      })
+      .generate();
+  }); // should write robots.txt
   
-  describe('default exclusion', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots())
-        .page(function(page) {
-          page.app = new mock.Site();
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
+  it('should write robots.txt with crawl delay', function(done) {
+    chai.kerouac.use(robots({ delay: 5 }))
+      .finish(function() {
+        expect(this.body).to.equal('User-agent: *\r\nDisallow:\r\nCrawl-delay: 5\r\n\r\n');
+        done();
+      })
+      .generate();
+  }); // should write robots.txt with crawl delay
   
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\n');
-    });
-  }); // default exclusion
+  it('should write robots.txt with fractional crawl delay', function(done) {
+    chai.kerouac.use(robots({ delay: 0.5 }))
+      .finish(function() {
+        expect(this.body).to.equal('User-agent: *\r\nDisallow:\r\nCrawl-delay: 0.5\r\n\r\n');
+        done();
+      })
+      .generate();
+  }); // should write robots.txt with fractional crawl delay
   
-  describe('default exclusion with crawl delay', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots({ delay: 5 }))
-        .page(function(page) {
-          page.app = new mock.Site();
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
+  it('should write robots.txt with sitemap', function(done) {
+    chai.kerouac.use(robots())
+      .request(function(page) {
+        page.locals = {};
+        page.locals.sitemaps = [
+          { url: '/sitemap.xml', fullURL: 'http://www.example.com/sitemap.xml' }
+        ];
+      })
+      .finish(function() {
+        expect(this.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\nSitemap: http://www.example.com/sitemap.xml\r\n');
+        done();
+      })
+      .generate();
+  }); // should write robots.txt with sitemap
   
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\nCrawl-delay: 5\r\n\r\n');
-    });
-  }); // default exclusion with crawl delay
+  it('should not write robots.txt with sitemap that lacks a full URL', function(done) {
+    chai.kerouac.use(robots())
+      .request(function(page) {
+        page.locals = {};
+        page.locals.sitemaps = [
+          { url: '/sitemap.xml' }
+        ];
+      })
+      .finish(function() {
+        expect(this.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\n');
+        done();
+      })
+      .generate();
+  }); // should not write robots.txt with sitemap that lacks a full URL'
   
-  describe('default exclusion with fractional crawl delay', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots({ delay: 0.5 }))
-        .page(function(page) {
-          page.app = new mock.Site();
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
-  
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\nCrawl-delay: 0.5\r\n\r\n');
-    });
-  }); // default exclusion with fractional crawl delay
-  
-  describe('default exclusion with sitemap', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots())
-        .page(function(page) {
-          page.app = new mock.Site();
-          page.app.pages = [
-            { url: '/sitemap.xml', fullURL: 'http://www.example.com/sitemap.xml', sitemap: true }
-          ];
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
-  
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\nSitemap: http://www.example.com/sitemap.xml\r\n');
-    });
-  }); // default exclusion with sitemap
-  
-  describe('default exclusion with sitemap in parent site, with mounted option', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots({ mounted: true }))
-        .page(function(page) {
-          page.app = new mock.Site();
-          page.app.parent = new mock.Site();
-          page.app.parent.pages = [
-            { url: '/sitemap.xml', fullURL: 'http://www.example.com/sitemap.xml', sitemap: true }
-          ];
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
-  
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\nSitemap: http://www.example.com/sitemap.xml\r\n');
-    });
-  }); // default exclusion with sitemap in parent site, with mounted option
-  
-  describe('default exclusion with sitemap that lacks a full URL', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots())
-        .page(function(page) {
-          page.app = new mock.Site();
-          page.app.pages = [
-            { url: '/sitemap.xml', sitemap: true }
-          ];
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
-  
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\n');
-    });
-  }); // default exclusion with sitemap that lacks a full URL
-  
-  describe('default exclusion with multiple sitemaps', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots())
-        .page(function(page) {
-          page.app = new mock.Site();
-          page.app.pages = [
-            { url: '/sitemap.xml', fullURL: 'http://www.example.com/sitemap.xml', sitemap: true },
-            { url: '/blog/sitemap.xml', fullURL: 'http://www.example.com/blog/sitemap.xml', sitemap: true }
-          ];
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
-  
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\nSitemap: http://www.example.com/sitemap.xml\r\nSitemap: http://www.example.com/blog/sitemap.xml\r\n');
-    });
-  }); // default exclusion with multiple sitemaps
-  
-  describe('default exclusion with multiple sitemaps and a sitemap index', function() {
-    var page, err;
-
-    before(function(done) {
-      chai.kerouac.use(robots())
-        .page(function(page) {
-          page.app = new mock.Site();
-          page.app.pages = [
-            { url: '/sitemap_index.xml', fullURL: 'http://www.example.com/sitemap_index.xml', sitemapIndex: true },
-            { url: '/sitemap.xml', fullURL: 'http://www.example.com/sitemap.xml', sitemap: true },
-            { url: '/blog/sitemap.xml', fullURL: 'http://www.example.com/blog/sitemap.xml', sitemap: true }
-          ];
-        })
-        .end(function(p) {
-          page = p;
-          done();
-        })
-        .dispatch();
-    });
-  
-    it('should write robots.txt', function() {
-      expect(page.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\nSitemap: http://www.example.com/sitemap_index.xml\r\n');
-    });
-  }); // default exclusion with multiple sitemaps and a sitemap index
+  it('should write robots.txt with multiple sitemaps', function(done) {
+    chai.kerouac.use(robots())
+      .request(function(page) {
+        page.locals = {};
+        page.locals.sitemaps = [
+          { url: '/sitemap.xml', fullURL: 'http://www.example.com/sitemap.xml' },
+          { url: '/blog/sitemap.xml', fullURL: 'http://www.example.com/blog/sitemap.xml' }
+        ];
+      })
+      .finish(function() {
+        expect(this.body).to.equal('User-agent: *\r\nDisallow:\r\n\r\nSitemap: http://www.example.com/sitemap.xml\r\nSitemap: http://www.example.com/blog/sitemap.xml\r\n');
+        done();
+      })
+      .generate();
+  }); // should write robots.txt with multiple sitemaps
   
 });
