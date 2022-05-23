c7b5c4b9f9a0fbc538dafce009f5e8e2e550e784
Jared Hanson <jaredhanson@gmail.com>
2022-05-23 15:27:49 -0700

    Add tests.

--- test/mapper.test.js
+++ test/mapper.test.js
@@ -4,4 +4,13 @@ var robotstxt = require('../lib');
 
 describe('Mapper', function() {
   
+  it('should request robots.txt', function(done) {
+    chai.kerouac.map(robotstxt.createMapper())
+      .finish(function() {
+        expect(this).to.request([ '/robots.txt' ]);
+        done();
+      })
+      .generate();
+  }); // should request robots.txt
+  
 });
