01ac5a11ca51f652f0d9ef9337fc294cb4f70ee2
Jared Hanson <jaredhanson@gmail.com>
2022-05-28 09:07:01 -0700

    Add tests.

--- test/mapper.test.js
+++ test/mapper.test.js
@@ -18,16 +18,57 @@ describe('Mapper', function() {
   
   it('should set sitemap for robots.txt', function(done) {
     chai.kerouac.map(robotstxt.createMapper(), [
-      { path: '/sitemap.xml', isSitemap: true }
+      { path: '/index.html' },
+      { fullURL: 'http://www.example.com/sitemap.xml', isSitemap: true }
     ])
       .close(function() {
         expect(this).to.request([ '/robots.txt' ]);
         expect(this.pages['/robots.txt'].locals).to.deep.equal({
-          sitemaps: [ { path: '/sitemap.xml', isSitemap: true } ]
+          sitemaps: [
+            { fullURL: 'http://www.example.com/sitemap.xml', isSitemap: true }
+          ]
         })
         done();
       })
       .generate();
   }); // should set sitemap for robots.txt
+  
+  it('should set multiple sitemaps for robots.txt', function(done) {
+    chai.kerouac.map(robotstxt.createMapper(), [
+      { path: '/index.html' },
+      { fullURL: 'http://www.example.com/sitemap.xml', isSitemap: true },
+      { fullURL: 'http://www.example.com/blog/sitemap.xml', isSitemap: true }
+    ])
+      .close(function() {
+        expect(this).to.request([ '/robots.txt' ]);
+        expect(this.pages['/robots.txt'].locals).to.deep.equal({
+          sitemaps: [
+            { fullURL: 'http://www.example.com/sitemap.xml', isSitemap: true },
+            { fullURL: 'http://www.example.com/blog/sitemap.xml', isSitemap: true }
+          ]
+        })
+        done();
+      })
+      .generate();
+  }); // should set multiple sitemaps for robots.txt
+  
+  it('should filter out sitemaps in a sitemap index for robots.txt', function(done) {
+    chai.kerouac.map(robotstxt.createMapper(), [
+      { path: '/index.html' },
+      { fullURL: 'http://www.example.com/sitemap.xml', isSitemap: true, isInSitemap: true },
+      { fullURL: 'http://www.example.com/blog/sitemap.xml', isSitemap: true, isInSitemap: true },
+      { fullURL: 'http://www.example.com/sitemap_index.xml', isSitemap: true }
+    ])
+      .close(function() {
+        expect(this).to.request([ '/robots.txt' ]);
+        expect(this.pages['/robots.txt'].locals).to.deep.equal({
+          sitemaps: [
+            { fullURL: 'http://www.example.com/sitemap_index.xml', isSitemap: true }
+          ]
+        })
+        done();
+      })
+      .generate();
+  }); // should filter out sitemaps in a sitemap index for robots.txt
   
 });
